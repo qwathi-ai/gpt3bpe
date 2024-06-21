1969c525578c173ceb87c4bc5955da827b3e060e
Yehudi Hollevoet <athenkosi@amile.io>
2024-06-22 00:56:08 +0200

    I am being naughty. No more commits to master please

--- src/sampling/mod.rs
+++
@@ -1,8 +0,0 @@
-fn fill (shape: [i32;2], filler: i32) -> Vec<Vec<i32>> {
-    let mut output = vec![];
-    for dimension in shape {
-        output.insert(vec![filler; dimension])
-    }
-    output
-}
-
